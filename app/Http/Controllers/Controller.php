be45d43d5dcc602935aaffef4564e150dcf9403c
Vang Huynh <vanghm@imt-soft.com>
2021-03-31 21:20:30 +0700

    add format response

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Carbon\Carbon;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
@@ -10,4 +11,29 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    protected function success($result, $statusCode = 200, $statusValue = 'OK') {
+        $response = [
+            'success' => true,
+            'data' => $result,
+            'statusCode' => $statusCode,
+            'statusValue' => $statusValue,
+            'executeDate' => Carbon::now()->toDateTimeLocalString()
+        ];
+
+        return response()->json($response);
+    }
+
+    protected function error($result, $statusCode = 500, $statusValue = 'ERROR') {
+        $response = [
+            'success' => false,
+            'data' => $result,
+            'statusCode' => $statusCode,
+            'statusValue' => $statusValue,
+            'executeDate' => Carbon::now()->toDateTimeLocalString()
+        ];
+
+        return response()->json($response);
+    }
+
 }
